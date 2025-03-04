3954ba68b0c94bb8eada9b9e26e98c66f0d94d45
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-03-04 22:56:23 +0900

    kadai_020の提出

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+// 変更するHTML要素を取得
+const text = document.getElementById('text');
+
+// トリガー(ボタン)になるHTML要素を取得
+const btn = document.getElementById('btn');
+
+// btnがクリックされたときにイベント処理を実行
+btn.addEventListener('click', () => {
+    // textの内容を変更
+    text.textContent = 'ボタンをクリックしました'
+})
